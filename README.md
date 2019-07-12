f608e18cab738cdac2c1bd544426735bf5527d6d
Ashusharma7 <ashu7073657271@gmail.com>
2019-07-12 19:09:25 +0530

    Initial bringup

---
+++ README.md
@@ -0,0 +1,3 @@
+# TWRP Tree for Realme U1
+
+* Minimal device tree for building PBRP for ``rmx1833``
---
+++ omni_rmx1833.mk
@@ -0,0 +1,21 @@
+# Release name
+PRODUCT_RELEASE_NAME := rmx1833
+
+# Inherit from the common Open Source product configuration
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base.mk)
+
+# Inherit from our custom product configuration
+$(call inherit-product, vendor/pb/config/common.mk)
+
+# Allow Missing Dependencies
+ALLOW_MISSING_DEPENDENCIES=true
+
+TARGET_SCREEN_HEIGHT := 1520
+TARGET_SCREEN_WIDTH := 720
+
+# Device identifier. This must come after all inclusions
+PRODUCT_DEVICE := rmx1833
+PRODUCT_NAME := omni_rmx1833
+PRODUCT_BRAND := Realme
+PRODUCT_MODEL := Realme U1
+PRODUCT_MANUFACTURER := Oppo
